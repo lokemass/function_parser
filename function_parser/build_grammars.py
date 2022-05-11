4f344e36a929a79619fc0f059d204ee39b230f97
Lokeshwar G <lokeshwar.g@openweaver.com>
2022-05-11 12:47:59 +0530

    version

--- function_parser/build_grammars.py
+++ function_parser/build_grammars.py
@@ -6,9 +6,9 @@ from tree_sitter import Language
 
 _GRAMMARs = {
     "go": ("https://github.com/tree-sitter/tree-sitter-go.git", "tree-sitter-go", "v0.13.3"),
-    "java": ("https://github.com/tree-sitter/tree-sitter-java.git", "tree-sitter-java", "v0.13.0"),
+    "java": ("https://github.com/tree-sitter/tree-sitter-java.git", "tree-sitter-java", "v0.19.1"),
     "javascript": ("https://github.com/tree-sitter/tree-sitter-javascript.git", "tree-sitter-javascript", "v0.19.0"),
-    "python": ("https://github.com/tree-sitter/tree-sitter-python.git", "tree-sitter-python", "v0.14.0"),
+    "python": ("https://github.com/tree-sitter/tree-sitter-python.git", "tree-sitter-python", "v0.19.0"),
 }
 
 # if __name__ == '__main__':
